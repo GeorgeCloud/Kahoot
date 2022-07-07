37ae4c71c5a1109092d08c1e4e62189849f52558
GeorgeCloud <net.george.sky@gmail.com>
2022-07-06 21:29:14 -0700

    assign uuid to io server for host

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const { Server }     = require("socket.io");
 const io             = new Server(server);
 const bodyParser     = require('body-parser');
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const uuid = require("uuid");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main', handlebars: allowInsecurePrototypeAccess(Handlebars) }));
@@ -18,21 +19,53 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-io.on('connection', (socket) => {
-  // console.log(socket.id)
-  io.emit('display message', 'a user connected');
-  console.log('a user connected');
+io.engine.generateId = (req) => {
+  return uuid.v4(); // must be unique across all Socket.IO servers
+}
 
+// User connected
+io.on('connection', (socket) => {
+  // Host creates server
+  socket.on('host game', () => {
+    const server_id = uuid.v4();
+
+    io.emit('server_id', server_id);
+  });
+
+  socket.on('join game', () => {
+    // connect user to correct io server
+    // io.emit('server_id', server_id);
+  });
+
+  // User disconnected
   socket.on('disconnect', () => {
     io.emit('display message', 'user disconnected');
     console.log('user disconnected')
-  });
-
-  socket.on('display message', (msg) => {
-    io.emit('display message', msg);
   });
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// socket.on('disconnect', () => {
+//   io.emit('display message', 'user disconnected');
+//   console.log('user disconnected')
+// });
+// io.emit('display message', 'a user connected');
+// console.log('a user connected');
